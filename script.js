a31a00157e24c5747f6c8219b5c6e9c3c78c911a
Andrew D'Ambrosio <adambrosio20@gmail.com>
2020-04-04 19:35:33 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// const today = moment();
+// console.log(today.format());
+
+// const moment = require('moment')
+// console.log("Today's date is: " + 
+// today.format('YYYY-MM-DD')
+// );
+
+// var date = moment().format("MMM Do YY");
+// $(".").append(date);
+
+// console.log(date);
+
+
